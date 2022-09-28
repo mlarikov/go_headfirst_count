0900b7b13f63112459764634a6a025c813fb4d66
Mikhail Larikov <mlarikov@gmail.com>
2022-09-28 17:21:01 +0300

    qwe

---
+++ go.mod
@@ -0,0 +1,5 @@
+module count
+
+go 1.18
+
+require github.com/mlarikov/go_headfirst_datafile v0.0.0-20220928133206-db930bf72a1a // indirect
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/mlarikov/go_headfirst_datafile/datafile"
+)
+
 func main() {
 	datafile.GetStrings()
 }
