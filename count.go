895a5e09978ae9cceae701c38706a890301fa8d2
Mikhail Larikov <mlarikov@gmail.com>
2022-09-28 17:29:56 +0300

    qwe

---
+++ count.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mlarikov/go_headfirst_datafile"
+)
+
+func main() {
+	go_headfirst_datafile.GetStrings()
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/mlarikov/go_headfirst_datafile/datafile"
-)
-
-func main() {
-	datafile.GetStrings()
-}
